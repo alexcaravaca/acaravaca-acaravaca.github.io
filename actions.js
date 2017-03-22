680c38dfeaec94157c6a3155bbc3ca3a32b2f3f3
alexcaravaca <alexcaravaca@gmail.com>
2017-03-22 14:40:52 -0600

    test

--- actions.js
+++ actions.js
@@ -9,15 +9,17 @@
 		  console.log("click");
  													
 				  $.ajax({
-				    url: 'https://demo-1.fico.site/data/api/entities/customer',
+				    url: 'https://demo-1.fico.site/data/api/entities/customer?callback=?',
 				    type: 'POST',
                     crossDomain: true,  
+                    jsonp: "callback",
                     dataType: 'json',
 				    data: {
 				        	"email":" devtest@prodigious ",
 							"customer_id":1234568904,
 							"patient_id":1234568904,
-							"address_line_1":"Birri de Santa Barbara"
+							"address_line_1":"Birri de Santa Barbara",
+                            format: "json"
 				    },
 				    headers: {
 				       
